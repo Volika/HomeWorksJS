faa94c6e31935fae7645a8d6b9c11e834dbee895
Valeriia Sopko <volikavs@gmail.com>
2025-01-29 16:22:58 +0100

    Add files via upload

--- lesson_02/js/script_05.js
+++ lesson_02/js/script_05.js
@@ -7,10 +7,11 @@ const secondInHour = 3600;
 const secondInMinute = 60;
 
 const dataInputHours = Math.floor(dataInput / secondInHour);
-const dataInputMinutes = Math.floor((dataInput - (dataInputHours * secondInHour)) / secondInMinute);
-const dataInputSecond = (dataInput - (dataInputHours * secondInHour) - (dataInputMinutes * secondInMinute));
+const dataInputMinutes = Math.floor(dataInput % secondInHour / secondInMinute);
+// const dataInputMinutes = Math.floor((dataInput - (dataInputHours * secondInHour)) / secondInMinute);
+const dataInputSecond = Math.floor(dataInput % secondInHour % secondInMinute);
 
-const lenThousand = dataInput / 1000;
+// const lenThousand = dataInput / 1000;
 
 document.write(`Введено кількість секунд, що пройшла від початку доби : ${dataInput} <br> <br>
                Це становить ${dataInputHours} год. : ${dataInputMinutes} хв. :  ${dataInputSecond} сек `

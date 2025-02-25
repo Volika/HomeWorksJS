827da8b354476552042be860e6f262aa61249436
Valeriia Sopko <volikavs@gmail.com>
2025-02-25 04:19:39 +0100

    06

--- lesson_08/js/script_806.js
+++ lesson_08/js/script_806.js
@@ -8,67 +8,7 @@ if (confirm('Почати тестування?')) {
    ●	складаються з більше ніш 5 символів.
 
 */
-
-// Функція отримує кількість номерів від користувача
-function getCarNumberCount() {
-   return parseInt(prompt(`Введіть кількість номерів автомобілів:`, "5")) || 1
-}
-
-// Функція отримує номери автомобілів від користувача
-function getCarNumbers(count) {
-   let numbers = []
-   for (let i = 0; i < count; i++) {
-      let number = prompt(`Введіть номер автомобіля №${i + 1}:`, "АВ12В")
-       numbers.push(number.trim()) // Видаляємо зайві пробіли і додаємо в масив
-   }
-   return numbers
-}
-
-// Функція підраховує номери, які починаються з "А"
-function countStartsWithA(numbers) {
-   let count = 0
-   for (let i = 0; i < numbers.length; i++) {
-       if (numbers[i][0].toUpperCase() === "А") { // Перевіряємо першу букву у верхньому регістрі
-         count++
-      }
-   }
-   return count
-}
-
-// Функція підраховує номери, де перша і остання літери співпадають
-function countSameFirstLast(numbers) {
-   let count = 0
-   for (let i = 0; i < numbers.length; i++) {
-      let num = numbers[i].toUpperCase() 
-      if (num[0] === num[num.length - 1]) { 
-         count++
-      }
-   }
-   return count
-}
-
-// Функція підраховує номери, які мають більше ніж 5 символів
-function countMoreThanFiveChars(numbers) {
-   let count = 0
-   for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i].length > 5) {
-         count++
-      }
-   }
-   return count
-}
-
-// Отримуємо кількість номерів
-let carNumberCount = getCarNumberCount()
-
-// Отримуємо номери від користувача
-let carNumbers = getCarNumbers(carNumberCount)
-
-// Підраховуємо необхідні категорії
-let startsWithA = countStartsWithA(carNumbers)
-let sameFirstLast = countSameFirstLast(carNumbers)
-let moreThanFiveChars = countMoreThanFiveChars(carNumbers)
-
+lesson_08/exercise_805.html
 // Виводимо результати
 
 document.write(`Номери  ${carNumberCount} автомобілів:<br>`)
